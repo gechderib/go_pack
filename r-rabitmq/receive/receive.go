460f439d0e4a86c6bf360817a5f6d03622452d66
gechderib <gechderib@gmail.com>
2026-04-19 18:18:04 +0300

    publish with context and worker

--- r-rabitmq/receive/receive.go
+++ r-rabitmq/receive/receive.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"time"
 
 	"github.com/rabbitmq/amqp091-go"
 )
@@ -37,6 +39,13 @@ func ReceiveMessage() {
 	)
 	failOnError(err, "Failed to declare a queue")
 
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
+
 	// consume messages
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -54,14 +63,20 @@ func ReceiveMessage() {
 	go func() {
 		for d := range msgs {
 			// ack	the message after processing
-			d.Ack(false) // Ack only this one message
-			// d.Ack(true)  // Ack this and all previous unacknowledged messages
+			// d.Ack(false) // Ack only this one message
+			// d.Ack(true) // Ack this and all previous unacknowledged messages
 
 			// // nack the message, requeue it for redelivery
 			// d.Nack(false, true) // Nack only this one message and requeue it
 			// d.Nack(true, true)  // Nack this and all previous unacknowledged messages and requeue them
 
 			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(true)
+
 		}
 	}()
 
--- r-rabitmq/send/send.go
+++ r-rabitmq/send/send.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/rabbitmq/amqp091-go"
@@ -39,8 +41,8 @@ func SendMessage() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	randomUserID := time.Now().Unix()
-	body := "Hello World! " + string(randomUserID)
+	// read the body from terminal
+	body := bodyFrom(os.Args)
 
 	err = ch.PublishWithContext(ctx,
 		"",     // exchange
@@ -57,6 +59,16 @@ func SendMessage() {
 
 	log.Printf(" [x] Sent %s", body)
 
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	return s
 }
 
 func main() {
